45f9e9b9346176219445efacab16fb6ea5eaacac
arashzich <arashzich1992@gmail.com>
2022-11-28 19:58:24 +0330

    seen 14

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from .models import Article
 
 # Create your views here.
+
+
 def home(request):
-	context = {
-		"articles": Article.objects.filter(status="p").order_by("-published")
-	}
-	return render(request, "blog/home.html", context)
+    context = {
+        "articles": Article.objects.filter(status="p").order_by("-published")
+    }
+    return render(request, "blog/home.html", context)
 
 
-def detail(request,slug):
-	context = {
-		"item": Article.objects.get(slug=slug)
-	}
-	return render(request, "blog/detail.html", context)
+def detail(request, slug):
+    context = {
+        "article": Article.objects.get(slug=slug)
+    }
+    return render(request, "blog/detail.html", context)
